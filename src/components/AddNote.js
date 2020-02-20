510c9aef15f7bd1dd65137748c42e3e151491a90
Ruchit <ruchit@getposhaq.com>
2020-02-20 11:29:39 +0530

    ruchit 1

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -7,72 +7,78 @@ import { NoteConsumer } from '../context'
 export default class AddNote extends Component { 
     
     render() {
-        const { handleClick , addnote } = this.props
+        // const { handleClick , addnote } = this.props
          return (
              
-            
-            <div className="white-box">
-               <form onSubmit={this.props.addnote}>
-                  <div className="form-group py-4 px-4">
-                    <span className="fa-text">
-                        <i  className="fa fa-file-text-o "/>
-                    </span>
-                    <span className="note-text">
-                        New Note
-                    </span>
-                    <div className="px-4">
-                       <label>Title</label>
-                       <input id="new-note-title"
-                       type ="text"
-                       className="form-control text-capitalize"
-                       placeholder="add a note item"
-                       onChange={this.handleClick}
-                      />
-                    </div>
-                    <div className="py-2 px-4">
-                        <label>Content</label>
-                        <input type="text"
-                        className="form-control text-capitalize"
-                        placeholder="add the content"
-                       
-                       onChange={this.handleClick}
-                       />
-                    </div>
-                    <div className="form-group px-4">
-                        <label>tags</label>
-                        <input id="new-tag-title"
-                        type="text"
-                        className="form-control text-capitalize"
-                        placeholder="add a tag"
-                        onChange={this.handleClick}
-                        />
-                    </div>
-                  </div>  
-              </form>
-               
-                <button id="button"
-                        className="btn-1" onClick={() => 
-                            addnote()
-                        }>
-                    <span>
-                        <i  className="fa fa-save"/>
-                        <span className=" px-2">
-                        Save
-                            </span>
-                    </span>
-                    </button>
-               
-                    <Link to="/">
-                    <button className="btn-2">
-                    <span>
-                        <i  className="fa fa-close"/>
-                        <span className=" px-2">
-                        cancel
-                            </span>
-                    </span>
-                    </button>
-                    </Link>
-            </div>
+            <NoteConsumer>
+                {(value)=>{
+                    return(
+                        <React.Fragment>
+                            <div className="white-box">
+                                <form onSubmit={console.log("onsubmit")}>
+                                    <div className="form-group py-4 px-4">
+                                        <span className="fa-text">
+                                            <i  className="fa fa-file-text-o "/>
+                                        </span>
+                                        <span className="note-text">
+                                            New Note
+                                        </span>
+                                        <div className="px-4">
+                                        <label>Title</label>
+                                        <input id="new-note-title"
+                                        type ="text"
+                                        className="form-control text-capitalize"
+                                        placeholder="add a note item"
+                                        onChange={value.handleClick}
+                                        />
+                                        </div>
+                                        <div className="py-2 px-4">
+                                            <label>Content</label>
+                                            <input type="text"
+                                            className="form-control text-capitalize"
+                                            placeholder="add the content"
+                                        
+                                        onChange={value.handleClick}
+                                        />
+                                        </div>
+                                        <div className="form-group px-4">
+                                            <label>tags</label>
+                                            <input id="new-tag-title"
+                                            type="text"
+                                            className="form-control text-capitalize"
+                                            placeholder="add a tag"
+                                            onChange={value.handleClick}
+                                            />
+                                        </div>
+                                    </div>  
+                                </form>
+                            
+                                <button id="button"
+                                        className="btn-1" onClick={ console.log("onclick")                                           
+                                        }>
+                                    <span>
+                                        <i  className="fa fa-save"/>
+                                        <span className=" px-2">
+                                        Save
+                                        </span>
+                                    </span>
+                                </button>
+                            
+                                <Link to="/">
+                                    <button className="btn-2">
+                                        <span>
+                                            <i  className="fa fa-close"/>
+                                            <span className=" px-2">
+                                            cancel
+                                            </span>
+                                        </span>
+                                    </button>
+                                </Link>
+                            </div>
+                        </React.Fragment>
+                    )
+                }}
+            </NoteConsumer>
             
             
         )
--- src/context.js
+++ src/context.js
@@ -18,28 +18,31 @@ class NoteProvider extends Component {
 
     readNote = e => {
       e.preventDefault();
-      console.log(e.target);
-      const title = e.currentTarget.firstElementChild.innerHTML;
-      const info = e.currentTarget.MiddleElementChild.innerHTML;
-      const tag = e.currentTarget.lastElementChild.innerHTML;
-      this.setState({ writingNote: false, currentRead: { title: title, info : info, tag : tag} }); 
+      // console.log(e.target);
+      // const title = e.currentTarget.firstElementChild.innerHTML;
+      // const info = e.currentTarget.MiddleElementChild.innerHTML;
+      // const tag = e.currentTarget.lastElementChild.innerHTML;
+      // this.setState({ writingNote: false, currentRead: { title: title, info : info, tag : tag} }); 
     }; 
     
-      handleClick = () => {
-        const newTitle = document.getElementById("new-note-title").value;
-        const newContent = document.getElementById("new-note-content").value;
-        const newTag = document.getElementById("new-tag-content").value;
+    handleClick = () => {
+        // const newTitle = document.getElementById("new-note-title").value;
+        // const newContent = document.getElementById("new-note-content").value;
+        // const newTag = document.getElementById("new-tag-content").value;
         // const newObject = { title: newTitle, note: newContent, tag : newTag};
-        console.log(newTitle,newContent,newTag,"trial")
+        // console.log(newTitle,newContent,newTag,"trial")
+        console.log("hello from context");
+        
       
         // this.setState({
         //   storedNotes: [...this.state.storedNotes, newObject]
         // });
-      };
+    };
   
-      writeNote = () => {
+    writeNote = () => {
         this.setState({writingNote: true})
-      }
+    };
+
     render() {
         return (
             <NoteContext.Provider value={{
